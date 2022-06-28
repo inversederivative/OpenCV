804b409bdf05e934b6daf2f98d3fdbd96e7795d5
cb7catalyst@gmail.com <Gordon**Hinckley7>
2022-06-28 16:29:26 -0400

    Added Starship.jpg, and made revisions to main.cpp to test the jpeg.

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,16 @@ int main()
 {
     // Location of Test Image
     std::string path = "../Resources/GunnaWunna.png";
+    std::string path2 = "../Resources/Starship.jpg";
+
     cv::Mat img = cv::imread(path); // Creating OpenCV "Image" object
+    cv::Mat img2 = cv::imread(path2);
 
     // Create a named Window
     cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
     // Show image
     cv::imshow("Display Image", img);
+    cv::imshow("Display Image", img2); // Only shows one image.. TODO: Fix me!
 
     // Wait forever (or until user closes window)
     cv::waitKey(0);
